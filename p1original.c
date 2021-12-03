6cabe09cbb92f76a850788eb54851d88851675fc
Dinesh Cherukuri <93107793+dineshcherukuri815@users.noreply.github.com>
2021-12-03 09:43:42 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,10 @@
-
-}
+#include<stdio.h>
+int main ()
+{
+  int a,b,c;
+  printf("entre two numbers\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("Sum is %d+%d is %d\n",a,b,c);
+return 0;
+} 
---
+++ p2original.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("entre value/n");
+scanf("%d,&a");
+return a;
+}
+int add (int a,int b)
+{
+int c;
+  c=a+b;
+  return c;
+ }
+int output (int a,int b,int sum)
+{
+printf("sum of a&b is c %d %d %d a,b,sum")
+}
+int main()
+a=input;
+b=intput;
+sum=add(a,b) 
+output(a,b,sum)
+return 0;
+  }
