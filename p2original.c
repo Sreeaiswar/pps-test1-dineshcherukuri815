a686381761232534101e29cdaf472e24d2174cf3
Dinesh Cherukuri <93107793+dineshcherukuri815@users.noreply.github.com>
2021-12-03 09:53:22 +0000

    done

--- p2original.c
+++ p2original.c
@@ -3,23 +3,25 @@ int input()
 {
   int a;
   printf("entre value/n");
-scanf("%d,&a");
-return a;
+  scanf("%d",&a);
+   return a;
 }
 int add (int a,int b)
 {
-int c;
+  int c;
   c=a+b;
   return c;
  }
 int output (int a,int b,int sum)
 {
-printf("sum of a&b is c %d %d %d a,b,sum")
+printf("Sum of %d and %d is %d",a,b,sum);
 }
 int main()
-a=input;
-b=intput;
-sum=add(a,b) 
-output(a,b,sum)
+{
+  int a,b,sum;
+a=input();
+b=input();
+sum=add(a,b); 
+output(a,b,sum);
 return 0;
-  }
+}
