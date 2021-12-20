af6674daeca503e43fc187d46cbed671dfe0e7f8
Dinesh Cherukuri <93107793+dineshcherukuri815@users.noreply.github.com>
2021-12-20 05:46:41 +0000

    p3 original

--- p3original.c
+++ p3original.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
-int input()
+void input(int *a,int *b)
 {
-  int num1;
   printf("entre num: ");
-  scanf("%d",&num1);
+  scanf("%d%d",a,b);
 }
 
-int add(int num1,int num2,int* sum)
+void add(int a,int b,int* sum)
 {
-  *sum =num1+num2;
+  *sum = a+b;
 }
-void output(int num1,int num2,int sum)
+void output(int a,int b,int sum)
 {
-  printf("the sum of %d and %d = %d",num1,num2,sum);
+  printf("the sum of %d and %d = %d",a,b,sum);
 }
 int main()
 {
-  int num1,num2,sum=0;
-  num1 = input();
-  num2 = input();
-  sum = add(num1,num2,&sum);
-  output(num1,num2,sum);
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
